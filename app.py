8ec7ef077d6e6fe6a11cc486391bb6457582db29
Ashish Waghmare <ashishWaghmare@gmail.com>
2019-01-17 20:01:05 +0530

    op

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ import os
 import http.server
 import socketserver
 server_base_class = socketserver.TCPServer
-class LocalTCPServer(socketserver.TCPServer):
-    "Only accepts requests from 14.143.243.78"
-    def verify_request(self, request, client_address):
-        print(client_address)
-        return (client_address[0] == '14.143.243.78')
-handler = http.server.SimpleHTTPRequestHandler
+
+class GetHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print(self.headers)
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+handler = GetHandler
 PORT = int(os.environ.get("PORT", 17995))
-httpd = LocalTCPServer(("",PORT), handler)
+httpd = socketserver.TCPServer(("",PORT), handler)
 print("serving at port::",PORT)
 httpd.serve_forever()
