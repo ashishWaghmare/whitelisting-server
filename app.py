30c976686350aca75f4626ee818745216773fe12
Ashish Waghmare <ashishWaghmare@gmail.com>
2019-01-17 18:38:11 +0530

    ed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 import os
 
-PORT = os.environ.get("PORT", 17995)
+PORT = int(os.environ.get("PORT", 17995))
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port::",PORT)
