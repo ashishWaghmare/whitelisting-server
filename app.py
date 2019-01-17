91ef3356328f082a55346fb7c3b1436408c5754f
Ashish Waghmare <ashishWaghmare@gmail.com>
2019-01-17 19:22:25 +0530

    whitelistening

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ server_base_class = socketserver.TCPServer
 class LocalTCPServer(socketserver.TCPServer):
     "Only accepts requests from 14.143.243.78"
     def verify_request(self, request, client_address):
+        print(client_address)
         return (client_address[0] == '14.143.243.78')
 handler = http.server.SimpleHTTPRequestHandler
 PORT = int(os.environ.get("PORT", 17995))
