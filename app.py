8d8b2dc4836a9657aae8b89e01048fae47677d06
Ashish Waghmare <ashishWaghmare@gmail.com>
2019-01-17 18:29:22 +0530

    new

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import socketserver
 PORT = 80
 
 Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketServer.TCPServer(("", PORT), Handler)
+httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port::",PORT)
 httpd.serve_forever()
